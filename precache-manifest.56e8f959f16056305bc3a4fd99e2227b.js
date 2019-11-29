472b10289a085f7976a75ecc5557140f356148ad
RaquelMarques97 <araquelfmarques@gmail.com>
2019-11-29 12:09:40 +0000

    Updates

---
+++ precache-manifest.56e8f959f16056305bc3a4fd99e2227b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9175777ed5c9e27b0e52c16dec4d33d3",
+    "url": "/christmasGame/index.html"
+  },
+  {
+    "revision": "12eef932ef14c5f01a8b",
+    "url": "/christmasGame/static/css/main.d2312f14.chunk.css"
+  },
+  {
+    "revision": "8223b5bfbc416caa50b3",
+    "url": "/christmasGame/static/js/2.82e21abe.chunk.js"
+  },
+  {
+    "revision": "12eef932ef14c5f01a8b",
+    "url": "/christmasGame/static/js/main.8dfa0201.chunk.js"
+  },
+  {
+    "revision": "69d213a96f1fae2809d2",
+    "url": "/christmasGame/static/js/runtime-main.88b27675.js"
+  }
+]);
